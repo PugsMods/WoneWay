94171c9f209e9edc22ea61dca4bf4f1f07206411
PutoPug <62641095+PutoPug@users.noreply.github.com>
2021-03-14 00:15:52 +0530

    Langs: Done

--- src/main/java/com/putopug/woneway/init/BlockItemBase.java
+++ src/main/java/com/putopug/woneway/init/BlockItemBase.java
@@ -3,13 +3,20 @@ package com.putopug.woneway.init;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 
 //
 //@author PutoPug
 //
 public class BlockItemBase extends BlockItem {
+
+    public static final net.minecraft.item.ItemGroup BLOCKS = new net.minecraft.item.ItemGroup("wonewayblocks") {
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(WoneWayBlocks.SEETHROUGH_DIAMOND_BLOCK.get());
+        }
+    };
     public BlockItemBase(Block block) {
-        super(block, new Item.Properties().group(ItemGroup.BREWING));
+        super(block, new Item.Properties().group(BLOCKS));
     }
 }
