ae389f47db99495e546691a1106462cda3da8499
PutoPug <62641095+PutoPug@users.noreply.github.com>
2021-04-03 22:59:13 +0530

    Bump Versions to BETA 3

--- README.md
+++ README.md
@@ -7,3 +7,9 @@
 [![Discord](https://img.shields.io/discord/773211530413867028?label=Discord%3A&style=for-the-badge)](https://discord.gg/geNRqMu5XW)
 # WoneWay
 A Minecraft mod which adds One way blocks
+
+# Code For Old Versions
+
+[1.15.2-1.0.0BETA-2](https://github.com/PugsMods/WoneWay/tree/0a412808d3d4427aa8182eaaedf634652299b073)
+
+[1.15.2-1.0.0BETA](https://github.com/PugsMods/WoneWay/tree/d942ae593cc424aae12b3c0c69324c3029f1b00e)
---
+++ src/main/java/com/putopug/woneway/BlockColor.java
@@ -0,0 +1,39 @@
+package com.putopug.woneway;
+
+import com.putopug.woneway.WoneWay;
+import com.putopug.woneway.init.WoneWayBlocks;
+import net.minecraft.client.renderer.color.BlockColors;
+import net.minecraft.client.renderer.color.ItemColors;
+import net.minecraft.world.FoliageColors;
+import net.minecraft.world.biome.BiomeColors;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ColorHandlerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = WoneWay.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class BlockColor {
+    @SubscribeEvent
+    public static void setupBlockColors(ColorHandlerEvent.Block event) {
+        net.minecraft.client.renderer.color.BlockColors colors = event.getBlockColors();
+        colors.register((state, world, pos, tint) -> world != null && pos != null ? BiomeColors.getAverageFoliageColor(world, pos) : FoliageColors.getDefaultColor(), WoneWayBlocks.SEETHROUGH_ACACIA_LEAVES.get());
+        colors.register((state, world, pos, tint) -> world != null && pos != null ? BiomeColors.getAverageFoliageColor(world, pos) : FoliageColors.getDefaultColor(), WoneWayBlocks.SEETHROUGH_BIRCH_LEAVES.get());
+        colors.register((state, world, pos, tint) -> world != null && pos != null ? BiomeColors.getAverageFoliageColor(world, pos) : FoliageColors.getDefaultColor(), WoneWayBlocks.SEETHROUGH_DARK_OAK_LEAVES.get());
+        colors.register((state, world, pos, tint) -> world != null && pos != null ? BiomeColors.getAverageFoliageColor(world, pos) : FoliageColors.getDefaultColor(), WoneWayBlocks.SEETHROUGH_JUNGLE_LEAVES.get());
+        colors.register((state, world, pos, tint) -> world != null && pos != null ? BiomeColors.getAverageFoliageColor(world, pos) : FoliageColors.getDefaultColor(), WoneWayBlocks.SEETHROUGH_OAK_LEAVES.get());
+        colors.register((state, world, pos, tint) -> world != null && pos != null ? BiomeColors.getAverageFoliageColor(world, pos) : FoliageColors.getDefaultColor(), WoneWayBlocks.SEETHROUGH_SPRUCE_LEAVES.get());
+        colors.register((state, world, pos, tint) -> world != null && pos != null ? BiomeColors.getAverageFoliageColor(world, pos) : FoliageColors.getDefaultColor(), WoneWayBlocks.SEETHROUGH_GRASS_BLOCK.get());
+    }
+
+    @SubscribeEvent
+    public static void setupItemColors(ColorHandlerEvent.Item event) {
+        ItemColors colors = event.getItemColors();
+        colors.register((stack, tint) -> FoliageColors.getDefaultColor(), WoneWayBlocks.SEETHROUGH_ACACIA_LEAVES_ITEM.get());
+        colors.register((stack, tint) -> FoliageColors.getDefaultColor(), WoneWayBlocks.SEETHROUGH_BIRCH_LEAVES_ITEM.get());
+        colors.register((stack, tint) -> FoliageColors.getDefaultColor(), WoneWayBlocks.SEETHROUGH_DARK_OAK_LEAVES_ITEM.get());
+        colors.register((stack, tint) -> FoliageColors.getDefaultColor(), WoneWayBlocks.SEETHROUGH_JUNGLE_LEAVES_ITEM.get());
+        colors.register((stack, tint) -> FoliageColors.getDefaultColor(), WoneWayBlocks.SEETHROUGH_OAK_LEAVES_ITEM.get());
+        colors.register((stack, tint) -> FoliageColors.getDefaultColor(), WoneWayBlocks.SEETHROUGH_SPRUCE_LEAVES_ITEM.get());
+        colors.register((stack, tint) -> FoliageColors.getDefaultColor(), WoneWayBlocks.SEETHROUGH_GRASS_BLOCK_ITEM.get());
+    }
+}
