69ee89f0f1e3910ca362dd91dc992f269690a694
PugzAreCute <me@pugzarecute.com>
2021-07-25 22:01:04 +0530

    Did something

--- WoneWay-Forge/src/main/java/com/pugzarecute/woneway/init/WoneWayBlocks.java
+++ WoneWay-Forge/src/main/java/com/pugzarecute/woneway/init/WoneWayBlocks.java
@@ -24,8 +24,8 @@
 package com.pugzarecute.woneway.init;
 
 import com.pugzarecute.woneway.WoneWay;
-iimport com.pugzarecute.woneway.init.WoneWayBlocks;
-        mport com.pugzarecute.woneway.blocks.SeethroughBlock;
+import com.pugzarecute.woneway.init.WoneWayBlocks;
+import com.pugzarecute.woneway.blocks.SeethroughBlock;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
