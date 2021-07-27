68af4d66b3e864d46dbd65fe25f61b3e268878fd
PugzAreCute <me@pugzarecute.com>
2021-07-27 15:31:28 +0530

    Allow mod to run

--- WoneWay-Forge/src/main/java/com/pugzarecute/woneway/ClientEventBusSub.java
+++ WoneWay-Forge/src/main/java/com/pugzarecute/woneway/ClientEventBusSub.java
@@ -31,7 +31,6 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fmlclient.registry.RenderingRegistry;
 
 @Mod.EventBusSubscriber(modid = WoneWay.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientEventBusSub {
