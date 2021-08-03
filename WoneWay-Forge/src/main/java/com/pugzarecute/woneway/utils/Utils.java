59bb596838293fa8fec3cc50ea4a40f24a927fbe
PugzAreCute <me@pugzarecute.com>
2021-08-03 17:49:52 +0530

    Key depression checker

--- WoneWay-Forge/src/main/java/com/pugzarecute/woneway/utils/Utils.java
+++ WoneWay-Forge/src/main/java/com/pugzarecute/woneway/utils/Utils.java
@@ -1,10 +1,16 @@
 package com.pugzarecute.woneway.utils;
 
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.lwjgl.glfw.GLFW;
 
 public class Utils {
+    private static final long WINDOW = Minecraft.getInstance().getWindow().getWindow();
+
     private static final Logger LOGGER = LogManager.getLogger();
 
     public static long startTimeMills;
@@ -17,7 +23,10 @@ public class Utils {
     public static void timeRan(){
         LOGGER.debug("Process has been running for "+(startTimeMills-System.currentTimeMillis())+"ms");
     }
-    public static void isKeyDepressed(int keyIn){
 
+    public static boolean isKeyDepressed(int keyIn){
+        if(FMLEnvironment.dist == Dist.CLIENT){
+            return InputConstants.isKeyDown(WINDOW,keyIn);
+        }else return false;
     }
 }
