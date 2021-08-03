f86f47e4780d01a613b013a025a7677bba1b4183
PugzAreCute <me@pugzarecute.com>
2021-08-03 16:43:36 +0530

    Begin working on timer

--- WoneWay-Forge/src/main/java/com/pugzarecute/woneway/Utils.java
+++ WoneWay-Forge/src/main/java/com/pugzarecute/woneway/Utils.java
@@ -5,8 +5,9 @@ import org.apache.logging.log4j.Logger;
 
 public class Utils {
     private static final Logger LOGGER = LogManager.getLogger();
+    public static long startTimeMills;
     public static void timerInit() {
+        startTimeMills = System.currentTimeMillis();
         LOGGER.debug("Timer started");
-        //TODO: timer
     }
 }
