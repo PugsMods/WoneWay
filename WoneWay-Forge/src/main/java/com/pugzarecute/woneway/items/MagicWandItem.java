d5ff9ab96d9c8b0da7aa6bbd7cd4434e8d8755a0
PugzAreCute <me@pugzarecute.com>
2021-08-11 20:53:35 +0530

    Nonnull

--- WoneWay-Forge/src/main/java/com/pugzarecute/woneway/items/MagicWandItem.java
+++ WoneWay-Forge/src/main/java/com/pugzarecute/woneway/items/MagicWandItem.java
@@ -25,13 +25,15 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
+import javax.annotation.Nonnull;
+
 public class MagicWandItem extends Item {
     public MagicWandItem() {
         super(new Item.Properties().tab(CreativeModeTab.TAB_MISC).durability(100).setNoRepair());
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level p_41432_, Player p_41433_, InteractionHand p_41434_) {
+    public InteractionResultHolder<ItemStack> use(@Nonnull Level p_41432_,@Nonnull Player p_41433_,@Nonnull InteractionHand p_41434_) {
         //TOdo: else scan chunk and highlight all blocks using opengl
         //Todo : Raytracing (OHNO)
         return super.use(p_41432_, p_41433_, p_41434_);
