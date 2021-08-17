4020dddaff8550aed2b0978626360241616fd24e
PugzAreCute <me@pugzarecute.com>
2021-08-17 21:33:29 +0530

    Debug message

--- WoneWay-Forge/src/main/java/com/pugzarecute/woneway/client/BlockColor.java
+++ WoneWay-Forge/src/main/java/com/pugzarecute/woneway/client/BlockColor.java
@@ -35,7 +35,10 @@ public class BlockColor {
     @SubscribeEvent
     public static void setupBlockColors(ColorHandlerEvent.Block event) {
         BlockColors colors = event.getBlockColors();
-        for (String leaves : WoneWayBlocks.LEAFMAP.keySet()) colors.register((state, world, pos, tint) -> world != null && pos != null ? BiomeColors.getAverageFoliageColor(world, pos) : FoliageColor.getDefaultColor(), WoneWayBlocks.LEAFMAP.get(leaves).get());
+        for (String leaves : WoneWayBlocks.LEAFMAP.keySet()){
+            colors.register((state, world, pos, tint) -> world != null && pos != null ? BiomeColors.getAverageFoliageColor(world, pos) : FoliageColor.getDefaultColor(), WoneWayBlocks.LEAFMAP.get(leaves).get());
+            WoneWay.LOGGER.debug("WoneWay: Registering BlockTint for "+leaves);
+        }
     }
 
     @SubscribeEvent
