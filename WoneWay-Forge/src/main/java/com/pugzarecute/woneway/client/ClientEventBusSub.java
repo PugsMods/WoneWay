3a58028ed39053e26c994b9f9180cb35c54044fc
PugzAreCute <me@pugzarecute.com>
2021-08-26 22:05:55 +0530

    Equality

--- WoneWay-Forge/src/main/java/com/pugzarecute/woneway/client/ClientEventBusSub.java
+++ WoneWay-Forge/src/main/java/com/pugzarecute/woneway/client/ClientEventBusSub.java
@@ -32,7 +32,8 @@ public class ClientEventBusSub {
 
     @SubscribeEvent
     public static void clientSetup(FMLClientSetupEvent event) {
-        for (String blockId : WoneWayBlocks.BLOCKMAP.keySet()) {
+        for (String blockId : WoneWayBlocks.BLOCKMAP.keySet())
+        {
             ItemBlockRenderTypes.setRenderLayer(WoneWayBlocks.BLOCKMAP.get(blockId).get(), RenderType.translucent());
             WoneWay.LOGGER.debug("WoneWay: Registering RenderType for "+blockId);
         }
