4c9f155eb4dfbdfd1ad9d10d3bc25dfb11fbd0a5
PugzAreCute <me@pugzarecute.com>
2021-08-26 21:59:10 +0530

    Equality

--- WoneWay-Forge/src/main/java/com/pugzarecute/woneway/utils/TimerRunnable.java
+++ WoneWay-Forge/src/main/java/com/pugzarecute/woneway/utils/TimerRunnable.java
@@ -19,7 +19,8 @@ package com.pugzarecute.woneway.utils;
 
 public class TimerRunnable implements Runnable{
     @Override
-    public void run() {
+    public void run()
+    {
         Utils.timeRan();
     }
 }
