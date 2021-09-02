0da9bcb8dbe31abc948541994bb5d47445229bdf
PugzAreCute <me@pugzarecute.com>
2021-09-02 22:02:15 +0530

    Stuff

---
+++ WoneWay-Forge/src/scripts/Recipies.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+# The WoneWay Recipe Generation Script!
+#
+# This script is used to automatically generate recipe files for woneway
+# blocks from the id list located in [This Folder]/generated/input_global.txt
+#
+# If you want to use this script, please remember the following things
+#
+# 1. This script must be runt from this directory only. If you run it from another folder, the
+# generated/input_global.txt must be located it that file.
+#
+# 2. This JSON will output everything to generated/recipes/.
+import re
+
+inputFilePath = "generated/input_global.txt"
+inputFile = open(inputFilePath)
+
+outputPath = "generated/recipies/"
+
+for x in inputFile:
+    x = x.splitlines()[0]
+    outFileName = outputPath + x + ".json"
+    print(outFileName)
+    outFile = open(outFileName, "w+")
+    print("""{
+    "type": "minecraft:crafting_shapeless",
+    "ingredients": [
+        {
+        "item": "minecraft:glass"
+        },
+        {
+            "item": "minecraft:""" + re.sub("seethrough_", "", x) + """"
+        }
+    ],
+    "result": {
+        "item": "woneway:""" + x + """",
+        "count": 2
+    }
+} 
+""")
+    outFile.write("""
+{
+    "type": "minecraft:crafting_shapeless",
+    "ingredients": [
+        {
+        "item": "minecraft:glass"
+        },
+        {
+            "item": "minecraft:""" + re.sub("seethrough_", "", x) + """"
+        }
+    ],
+    "result": {
+        "item": "woneway:""" + x + """",
+        "count": 2
+    }
+} 
+""")
+    outFile.close()
+inputFile.close()
