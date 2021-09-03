ac069f5da6ea7d715ba8a8fc8dea9c53dcedb8bf
PugzAreCute <me@pugzarecute.com>
2021-09-03 21:18:55 +0530

    Stuff

---
+++ WoneWay-Forge/src/scripts/Model.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# The WoneWay Model Generation Script!
+#
+# This script is used to automatically generate model files for woneway
+# blocks from the id list located in [This Folder]/generated/input_global.txt
+#
+# If you want to use this script, please remember the following things
+#
+# TO BE FILLED BY AUTHOR.
