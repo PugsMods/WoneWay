1fdb3efd8c7ea074c9dd150554d757d0d3fe66ff
PugzAreCute <me@pugzarecute.com>
2021-09-05 15:15:51 +0530

    Formattin

--- WoneWay-Forge/src/scripts/Blockstates.py
+++ WoneWay-Forge/src/scripts/Blockstates.py
@@ -13,7 +13,6 @@
 # 2. This JSON will output everything to generated/blockstates/.
 #
 # 3. You will get an FileNotFound error if generated/blockstates/ dosent exist, so im adding a placeholder file.
-import re
 
 inputFilePath = "generated/input_global.txt"
 inputFile = open(inputFilePath)
@@ -28,26 +27,26 @@ for x in inputFile:
     outFile.write("""{
   "variants": {
     "facing=north": {
-      "model": "woneway:block/"""+x+""""
+      "model": "woneway:block/""" + x + """"
     },
     "facing=east": {
-      "model": "woneway:block/"""+x+"""",
+      "model": "woneway:block/""" + x + """",
       "y": 90
     },
     "facing=south": {
-      "model": "woneway:block/"""+x+"""",
+      "model": "woneway:block/""" + x + """",
       "y": 180
     },
     "facing=west": {
-      "model": "woneway:block/"""+x+"""",
+      "model": "woneway:block/""" + x + """",
       "y": 270
     },
     "facing=up": {
-      "model": "woneway:block/"""+x+"""",
+      "model": "woneway:block/""" + x + """",
       "x": 270
     },
     "facing=down": {
-      "model": "woneway:block/"""+x+"""",
+      "model": "woneway:block/""" + x + """",
       "x": 90
     }
   }
