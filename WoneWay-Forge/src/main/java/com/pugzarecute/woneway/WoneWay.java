cd8ce350512981bb5289d7defcb2672ea26759e8
PugzAreCute <me@pugzarecute.com>
2021-10-03 08:50:51 +0530

    Remove unused Import

--- WoneWay-Forge/src/main/java/com/pugzarecute/woneway/WoneWay.java
+++ WoneWay-Forge/src/main/java/com/pugzarecute/woneway/WoneWay.java
@@ -19,8 +19,6 @@ package com.pugzarecute.woneway;
 
 import com.pugzarecute.woneway.init.WoneWayBlocks;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
