b315839b2b23e09fff20ee1e530b48e29549a29b
PugzAreCute <me@pugzarecute.com>
2021-10-22 21:42:24 +0530

    Begin move

---
+++ src/main/java/com/pugzarecute/woneway/client/ClientEventBusSub.java
@@ -0,0 +1,40 @@
+/*
+ * This file is part of WoneWay.
+ *
+ * WoneWay is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * WoneWay is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with WoneWay.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+//BROKEN CLASS DUE TO 1.17.1 (The Class is "Broken" because of 1.17)
+package com.pugzarecute.woneway.client;
+
+import com.pugzarecute.woneway.WoneWay;
+import com.pugzarecute.woneway.init.WoneWayBlocks;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod.EventBusSubscriber(modid = WoneWay.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ClientEventBusSub {
+
+    @SubscribeEvent
+    public static void clientSetup(FMLClientSetupEvent event) {
+        for (String blockId : WoneWayBlocks.BLOCKMAP.keySet()) {
+            ItemBlockRenderTypes.setRenderLayer(WoneWayBlocks.BLOCKMAP.get(blockId).get(), RenderType.translucent());
+            WoneWay.LOGGER.debug("WoneWay: Registering RenderType for " + blockId);
+        }
+    }
+}
