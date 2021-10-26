cd119eb615b6e8d40c3c1039f59db3a9ca4dbaa9
PugzAreCute <me@pugzarecute.com>
2021-10-26 14:08:54 +0530

    Test something

---
+++ src/main/java/com/pugzarecute/woneway/client/BlockColor.java
@@ -0,0 +1,53 @@
+/*
+ * This file is part of WoneWay.
+ *
+ * WoneWay is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * WoneWay is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with WoneWay.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+//TODO : blockcolors in 1.17.1
+
+package com.pugzarecute.woneway.client;
+
+import com.pugzarecute.woneway.WoneWay;
+import com.pugzarecute.woneway.init.WoneWayBlocks;
+import net.minecraft.client.color.block.BlockColors;
+import net.minecraft.client.color.item.ItemColors;
+import net.minecraft.client.renderer.BiomeColors;
+import net.minecraft.world.level.FoliageColor;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ColorHandlerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = WoneWay.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class BlockColor {
+
+    @SubscribeEvent
+    public static void setupBlockColors(ColorHandlerEvent.Block event) {
+        final BlockColors colors = event.getBlockColors();
+        for (String leaves : WoneWayBlocks.LEAFMAP.keySet()) {
+            colors.register((state, world, pos, tint) -> world != null && pos != null ? BiomeColors.getAverageFoliageColor(world, pos) : FoliageColor.getDefaultColor(), WoneWayBlocks.LEAFMAP.get(leaves).get());
+            WoneWay.LOGGER.debug("WoneWay: Registering BlockTint for " + leaves);
+        }
+    }
+
+    @SubscribeEvent
+    public static void setupItemColors(ColorHandlerEvent.Item event) {
+        final ItemColors colors = event.getItemColors();
+        for (String leaves : WoneWayBlocks.LEAFMAP.keySet()) {
+            colors.register((stack, tint) -> FoliageColor.getDefaultColor(), WoneWayBlocks.LEAFMAP.get(leaves).get());
+            WoneWay.LOGGER.debug("WoneWay: Registering ItemTint for " + leaves);
+        }
+    }
+}
---
+++ src/scripts/Blockstates.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+# The WoneWay Blockstate Generation Script!
+#
+# This script is used to automatically generate blockstate files for woneway
+# blocks from the id list located in [This Folder]/generated/input_global.txt
+#
+# If you want to use this script, please remember the following things
+#
+# 1. This script must be runt from this directory only. If you run it from another folder, the
+# generated/input_global.txt must be located it that directory.
+#
+# 2. This JSON will output everything to generated/blockstates/.
+#
+# 3. You will get an FileNotFound error if generated/blockstates/ dosent exist, so im adding a placeholder file.
+
+inputFilePath = "generated/input_global.txt"
+inputFile = open(inputFilePath)
+
+outputPath = "generated/blockstates/"
+
+for x in inputFile:
+    x = x.splitlines()[0]
+    outFileName = outputPath + x + ".json"
+    outFile = open(outFileName, "w+")
+    print("Generating JSON for " + x)
+    outFile.write("""{
+  "variants": {
+    "facing=north": {
+      "model": "woneway:block/""" + x + """"
+    },
+    "facing=east": {
+      "model": "woneway:block/""" + x + """",
+      "y": 90
+    },
+    "facing=south": {
+      "model": "woneway:block/""" + x + """",
+      "y": 180
+    },
+    "facing=west": {
+      "model": "woneway:block/""" + x + """",
+      "y": 270
+    },
+    "facing=up": {
+      "model": "woneway:block/""" + x + """",
+      "x": 270
+    },
+    "facing=down": {
+      "model": "woneway:block/""" + x + """",
+      "x": 90
+    }
+  }
+}
+""")
+    outFile.close()
+inputFile.close()
