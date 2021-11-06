3817a12cde38bac393d9542aeceb84f8ee6a1666
PugzAreCute <me@pugzarecute.com>
2021-11-06 18:41:54 +0530

    Update copyright.

--- WoneWay-Forge/src/scripts/Recipies.py
+++ WoneWay-Forge/src/scripts/Recipies.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
 
+#  This file is part of WoneWay.
+#
+#  WoneWay is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  WoneWay is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with WoneWay.  If not, see <https://www.gnu.org/licenses/>.
+
 # The WoneWay Recipe Generation Script!
 #
 # This script is used to automatically generate recipe files for woneway
--- src/main/java/com/pugzarecute/woneway/WoneWay.java
+++ src/main/java/com/pugzarecute/woneway/WoneWay.java
@@ -2,16 +2,16 @@
  * This file is part of WoneWay.
  *
  * WoneWay is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * WoneWay is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with WoneWay.  If not, see <https://www.gnu.org/licenses/>.
  */
 
@@ -30,4 +30,4 @@ public class WoneWay {
     public WoneWay() {
         WoneWayBlocks.init();
     }
-}rfkhsrfioweyhr8owyeur9w4u95u3498u3495uertgrioejgoirejgoiregjroeijgfrogergoijrgoijwpwerkowekfpkdpogpdfogkpfdgkodpgkfdpkdfpokpgdkgopdkgpdkgpddpgdpgpdogdpgdpgpdkgpdkgpgpkgdpgkpdfogkpdgkpdfgkpdfgpfgdgkkkkkkkkkkkkkkkkkkkkkkpokpkpertkpretkgpreti4-05o34-5o-04ot=5t6o-pyh=-pgh=-mn0g=-n0o=-h0=rh=-oel0[kerto;lemrjgklrmgklfdgjfdgmpdojktrporkigporeit-0[4ei6=3469o=54-69o5=-7ki6t-[0ik]]]rilgjotfphiojpfgohkfgpohkfgpohkfgphkofpohkpfghkfghkpfghkfgphkfgpohkfpgohkpghkpk[ol[p[ptr[phortp]hltr[jhkohkfgpohkfgphkopfgohkpfgohkpori-riy0-rtyi0-rtyi-rt0hkipfgojmghjg]]]]jtfhfhfglkhjeoiuy0ep9yghl;jgh[pjogkhj[npgkhj[npogykh[utyolj=-yuklj[p,hmhg,mh;gljghjghkjhkhjk]]]]]orjeoitjpie0utj00934ujt0934jh50934tju09ut0rgjudgfdgn,dmng,jerntio3w4jh09ptu34934i-t56ui4t69-i3t9-4i5-034it-0ir9dpjgoidnvlcboidfhj;jkfriotgju09u09urgjblfkdnblkcnblkcblkdlkgjeroptp9ert0-45-03495-068y409568u50-i95-3495itproejtgriuydgt7tft7er87fytr87tyr9etygre9gy898ytw9yrw9eoryuewoifusdfjsdhskfhskjfhskjfwieryw9yt98ty08tu0943kjhuierthreyth984u5y025784yu579w4rghukesgrshkfgbskdhfguiskyrw4y954u709574093ruwoiruewoirshdkfjhsdfhsdjfhsdkfh89woeyr50948u57024357804395tu8eroitujrelktjgfdklgjdlfkgoietu409u340984w-0tu4093tuwotusilfjhslgjfdklglkfdgnfjkdgh9ertureutrioyurtioyurtoiyhriohgfjlkhjflkdjlgu9sou0347y0w4t87ws4-t07u984yutlirehgshghsldkghseorptu4093ut04958u-4548309p5tjum4nrte8njklrey79huionjkregtb0opkmltrg98oijretroeitkgdlkfgjpfodiup0er9tup09456809385=3495-3469-0yitrpojkgkgergergfhklfgjhfgjhfgjlkjklfjklhsutr8urjhfgdwe8ry7432034782039584309684u35094u3jriwehrfuivkjsbdvjsmndbfsgduiwtryt408oeurt0pwdfngjvkhyuietw85y73409535869i8450-yigfjdlkhndf,ngdfmgslkrjtileutg9oruy09356u4098409680934568-468i54096ipoykrt;lhmfgkhj;fhj;fglhj;flgjh;rtjpyu9456u-486i-0456i8-054iyptokprtyir-069854-06954-6945-6=045=7-9540=654=-6y07y;klrtjkyhftgl;mhfg;lhmfghk;rltykp0iy60-45y6ilrjkbhrtuiy8yrtutjytjtyjtdyjtyjjfghgfgfhfg,fngfdghfuiodghodifghiodfhgoidfhgoidfhgoe8ryht09u49-t5-0i=-456=56i0549i-3458345i-49-4059-43059-34950-345940-3itpoertipoeritpoeritpreotpoiertiporeig;fdgkdf;lgkd;flgkd;flg;kldfgk;lfd;lgk;ldfglkdf;lgdsfghsuywter872t937y439tu54t0843509843509t4u7r83ture0t98rue0gre0gr0e8greg0re8gr0egujrklengrekngrejnytkletoi43ut6dxcbvudsifgsidufhsidfhsdfguhfidghdgvhfdigvhfuidvbhfgdgvuihsr9f8wyh9ru8w0ru4w0395u30495802940935i093468409583409540935tu8wujrteiteriotuoeiruteroitgjiertujerotuertigjeriojgdfgjdlgnbvcnkjcnbkjcvnbjknbkjgryujertiue09yt59i6-4693-049o5-59=350=345t046-95460-yigrtojgtrngfmhnfg,hfn,h,fgn,dfgidjpyrtkyportiyhpofihfipohfipuirpyukortpyokportr43y528735230709409214u72937592948130-82-48i2309ruwe8ruweoirjhewioruqpuroeiytioret
+}
